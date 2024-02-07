c4a6400cf80fb3a038114a17674310ba984000e4
aasanakey <sanakeyaugustineayiku@gmail.com>
2024-02-07 16:05:01 +0000

    feat: protect dashboard routes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,28 +1,33 @@
 'use client';
 import { Poppins } from "next/font/google";
 import "../globals.css";
-import { ThemeModeScript } from 'flowbite-react';
-import { Toaster } from 'react-hot-toast';
 import SideDrawer from "@/components/SideDrawer";
 import TopNavbar from "@/components/TopNavbar";
-import { useState } from "react";
-import { AppProvider } from "@/context/appContext";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAppState } from "@/context/appContext";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export default function DashboardLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     const [isOpen, setIsOpen] = useState(false)
+    const {isAuthenticated } = useAppState();
+    const router = useRouter();
+
+    useEffect(() => {
+      if(!isAuthenticated){
+        router.push('/')
+      }
+    }, [isAuthenticated])
+    
     return (
-        
-                <AppProvider>
-                    <div className="flex min-h-screen">
-                        <SideDrawer isDrawerOpen={isOpen} toggleDrawer={() => setIsOpen(!isOpen)} />
-                        <div className="flex flex-col min-h-screen flex-1 bg-[#FAFAFA]">
-                            <TopNavbar toggleDrawer={() => setIsOpen(!isOpen)} />
-                            {children}
-                        </div>
-                    </div>
-                    <Toaster />
-                </AppProvider>
+
+        <div className="flex min-h-screen">
+            <SideDrawer isDrawerOpen={isOpen} toggleDrawer={() => setIsOpen(!isOpen)} />
+            <div className="flex flex-col min-h-screen flex-1 bg-[#FAFAFA]">
+                <TopNavbar toggleDrawer={() => setIsOpen(!isOpen)} />
+                {children}
+            </div>
+        </div>
     )
 }
